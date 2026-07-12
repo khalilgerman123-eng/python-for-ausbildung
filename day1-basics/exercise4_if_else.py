4021e3b1a263c7e8a94ea153399b64cda332df89
Khalil Mostafa <khalilgerman123@gmail.com>
2026-07-12 09:41:36 +0300

    Day 1: if else conditions exercise
    Practicing conditional logic using if/else statements to check if a user is an adult based on their age input.

---
+++ day1-basics/exercise4_if_else.py
@@ -0,0 +1,8 @@
+# Day 1 - Exercise 4: If/else conditions
+age = input("How old are you? ")
+age = int(age)
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are not an adult yet")
